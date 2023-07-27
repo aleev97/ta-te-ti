74b5d60d0c5d9d5ee0dd360f7348d37e68a7ea0c
aleev97 <valverdealejandro327@gmail.com>
2023-07-27 19:22:47 -0300

    codigo refactorizado

--- src/App.jsx
+++ src/App.jsx
@@ -5,71 +5,62 @@ import { TURNS } from "./constant";
 import { checkWinner, checkEndGame, makeComputerMove } from "./assets/logic/board";
 import { WinnerModal } from "./components/WinnerModal";
 
+const INITIAL_BOARD = Array(9).fill(null);
+
 function App() {
-  // Estados
   const [board, setBoard] = useState(() => {
-    const boardFromStorage = window.localStorage.getItem("board");
-    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null);
+    const boardFromStorage = JSON.parse(window.localStorage.getItem("board"));
+    return boardFromStorage || INITIAL_BOARD;
   });
 
   const [turn, setTurn] = useState(() => {
-    const turnFormStorage = window.localStorage.getItem("turn");
-    return turnFormStorage ?? TURNS.x;
+    const turnFromStorage = window.localStorage.getItem("turn");
+    return turnFromStorage || TURNS.x;
   });
 
-  const [winner, setWinner] = useState(null); // null(no hay ganador); false(empate)
-  const [isAgainstComputer, setIsAgainstComputer] = useState(true); // Nuevo estado para indicar si se juega contra la computadora o no
+  const [winner, setWinner] = useState(null);
+  const [isAgainstComputer, setIsAgainstComputer] = useState(true);
 
-  // Efecto para el turno de la computadora
   useEffect(() => {
     if (isAgainstComputer && turn === TURNS.o && !winner) {
-      // Es el turno de la computadora (O) y no hay ganador
-      const delay = Math.random() * 1500 + 500; // Agrega un pequeño retraso para que la jugada de la computadora sea más natural
+      const delay = Math.random() * 1500 + 500;
       const computerMoveTimer = setTimeout(() => {
-        const newBoard = makeComputerMove([...board], TURNS.o); // TURNS.o representa la computadora
+        const newBoard = makeComputerMove([...board], TURNS.o);
         setBoard(newBoard);
-
-        // Cambiar el turno
         setTurn(TURNS.x);
-
-        // Revisar si hay ganador
         const newWinner = checkWinner(newBoard);
         if (newWinner) {
           confetti();
           setWinner(newWinner);
         } else if (checkEndGame(newBoard)) {
-          setWinner(false); // Empate
+          setWinner(false);
         }
       }, delay);
 
-      return () => clearTimeout(computerMoveTimer); // Limpiar el temporizador si el componente se desmonta antes de que se ejecute la jugada de la computadora
+      return () => clearTimeout(computerMoveTimer);
     }
   }, [board, turn, winner, isAgainstComputer]);
 
-  // Función para manejar el clic en una casilla
   const updateBoard = (index) => {
-    if (board[index] || winner) return; // Si la casilla está llena o hay un ganador, no hacer nada
+    if (board[index] || winner) return;
 
     const newBoard = [...board];
     newBoard[index] = turn;
     setBoard(newBoard);
 
-    // Cambiar el turno
     setTurn(turn === TURNS.x ? TURNS.o : TURNS.x);
 
-    // Revisar si hay ganador
     const newWinner = checkWinner(newBoard);
     if (newWinner) {
       confetti();
       setWinner(newWinner);
     } else if (checkEndGame(newBoard)) {
-      setWinner(false); // Empate
+      setWinner(false);
     }
   };
 
-  // Función para reiniciar el juego
   const resetGame = () => {
-    setBoard(Array(9).fill(null));
+    setBoard(INITIAL_BOARD);
     setTurn(TURNS.x);
     setWinner(null);
 
@@ -77,13 +68,11 @@ function App() {
     window.localStorage.removeItem("turn");
   };
 
-  // Función para cambiar el modo del juego
   const toggleGameMode = () => {
     setIsAgainstComputer((prev) => !prev);
-    resetGame(); // Reiniciar el juego cuando se cambie el modo
+    resetGame();
   };
 
-  // Guardar el estado del tablero y el turno en localStorage
   useEffect(() => {
     window.localStorage.setItem("board", JSON.stringify(board));
     window.localStorage.setItem("turn", turn);
@@ -98,9 +87,9 @@ function App() {
       </button>
 
       <section className="game">
-        {board.map((_, index) => (
+        {board.map((value, index) => (
           <Square key={index} index={index} updateBoard={updateBoard}>
-            {board[index]}
+            {value}
           </Square>
         ))}
       </section>
--- src/assets/logic/board.js
+++ src/assets/logic/board.js
@@ -2,44 +2,34 @@ import { WINNER_COMBOS } from "../../constant";
 
 // Función para comprobar si hay un ganador
 export const checkWinner = (boardToCheck, player) => {
-  // revisar combinaciones ganadoras
   for (const combo of WINNER_COMBOS) {
     const [a, b, c] = combo;
-    if (
-      boardToCheck[a] &&
-      boardToCheck[a] === boardToCheck[b] &&
-      boardToCheck[a] === boardToCheck[c]
-    ) {
+    if (boardToCheck[a] && boardToCheck[a] === boardToCheck[b] && boardToCheck[a] === boardToCheck[c]) {
       return boardToCheck[a];
     }
   }
-
   return null;
 };
 
 // Función para comprobar si el juego ha terminado en empate
-export const checkEndGame = (newBoard) => {
-  // reviso si hay empate
-  // reviso si no hay espacios vacios en el tablero
-  return newBoard.every((Square) => Square !== null);
-};
+export const checkEndGame = (newBoard) => newBoard.every((square) => square !== null);
 
 // Función para que la computadora realice su movimiento
 export const makeComputerMove = (board) => {
-    // Buscar un espacio vacío en el tablero
-    const emptyIndices = [];
-    for (let i = 0; i < board.length; i++) {
-      if (board[i] === null) {
-        emptyIndices.push(i);
-      }
+  // Buscar un espacio vacío en el tablero
+  const emptyIndices = board.reduce((indices, square, index) => {
+    if (square === null) {
+      indices.push(index);
     }
-  
-    // Elegir un índice al azar de los espacios vacíos
-    const randomIndex = Math.floor(Math.random() * emptyIndices.length);
-  
-    // Realizar el movimiento de la computadora en el tablero
-    const computerMoveIndex = emptyIndices[randomIndex];
-    board[computerMoveIndex] = '🟢'; 
-  
-    return board;
-  };
+    return indices;
+  }, []);
+
+  // Elegir un índice al azar de los espacios vacíos
+  const randomIndex = Math.floor(Math.random() * emptyIndices.length);
+
+  // Realizar el movimiento de la computadora en el tablero
+  const computerMoveIndex = emptyIndices[randomIndex];
+  board[computerMoveIndex] = '🟢';
+
+  return board;
+};
